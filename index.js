d21036220e4d3f14788d5f97168821e818cc97b5
Ambika.V <ambikavenkatesh1997@gmail.com>
2022-07-17 13:39:10 +0530

    Storing the data in the file

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const fs = require('fs');
 const app = express();
+
+app.use(express.json())
+
 app.get('/hello',(req,res) => {
     res.send("Hello World..")
 })
-
-
+app.post('/student',(req,res) => {
+    console.log(req.body)
+    let data = JSON.stringify(req.body)
+    fs.appendFile('student.txt', data,(err) =>{
+        if(err){
+            console.log("Error..")
+        }
+        else{
+            res.send("Done and Saved..")
+        }
+    })
+    
+})
 app.listen(4000,() =>{
     console.log('Server created..')
 })
